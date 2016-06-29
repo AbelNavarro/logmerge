48ef6b2e5271ebfc24a164d55be507cd9f1fbbd1
Abel Navarro <abel.navarro@gmail.com>
2016-06-29 10:23:45 +0200

    skeleton reading files

--- logmerge.py
+++ logmerge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse
+import sys
 
 def parse_args(args):
     parser = argparse.ArgumentParser(
@@ -12,7 +13,22 @@ def parse_args(args):
 
 
 def main():
-    parser = parse_args(sys.argv[1:])
+    res = parse_args(sys.argv[1:])
+
+    if res == argparse.Namespace():
+        print 'usage'
+        return 0
+    
+    if len(res.files) < 2:
+        print 'Need at least two files to merge'
+        return 1
+   
+    files = []
+    for file in res.files:
+        files.append(open(file, 'r'))
+
+    for file in files:
+        print file.readline()
 
 
 if __name__ == '__main__':
