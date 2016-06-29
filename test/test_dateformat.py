c66fc55f834661267eae7fd5d188c1d2cf41a96b
Abel Navarro <abel.navarro@gmail.com>
2016-06-29 16:41:12 +0200

    fixed rest of the tests

--- test/test_dateformat.py
+++ test/test_dateformat.py
@@ -9,34 +9,34 @@ class TestDateformat(unittest.TestCase):
 
     def test_empty(self):
         res = get_dateformat('')
-        print res
+        self.assertEquals(res, 'empty')
 
 
     def test_ovs(self):
         res = get_dateformat('2016-06-02T20:39:51.876Z|')
-        print res
+        self.assertEquals(res, 'ovs')
 
 
     def test_neutron(self):
         res = get_dateformat('2016-06-02 20:42:23.325 0000')
-        print res
+        self.assertEquals(res, 'nova')
 
 
     def test_chefclient(self):
         res = get_dateformat('[2016-06-02T19:58:58+00:00] INFO')
-        print res
+        self.assertEquals(res, 'chefclient')
 
 
     def test_messages(self):
         res = get_dateformat('2016-06-27T06:00:11.386456+00:00 hostname')
-        print res
+        self.assertEquals(res, 'messages')
 
 
     def test_novacompute(self):
         res = get_dateformat('2016-06-02 20:45:28.166 0000')
-        print res
+        self.assertEquals(res, 'nova')
 
 
     def test_pacemaker(self):
         res = get_dateformat('Jun 26 06:00:22 [13084]')
-        print res
+        self.assertEquals(res, 'pacemaker')
