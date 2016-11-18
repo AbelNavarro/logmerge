ec1fcd9600bdf7bf3888d4f86f5ddfd2e12142c7
Adam Spiers <logmerge@adamspiers.org>
2016-11-18 23:07:35 +0000

    use less cryptic "opts" variable
    "res" is presumably short for "result", but that doesn't indicate
    anything about the value other than it was obtained from calling
    something else.  So change the variable name to "opts" to make it
    clearer that it's for storing the CLI options.

--- test/test_argparse.py
+++ test/test_argparse.py
@@ -9,56 +9,54 @@ from logmerge import parse_args
 class TestArgparse(unittest.TestCase):
 
     def test_empty(self):
-        res = parse_args([])
-        self.assertEquals(res, argparse.Namespace(print_linenum=False))
+        opts = parse_args([])
+        self.assertEquals(opts, argparse.Namespace(print_linenum=False))
 
 
     def test_onefile(self):
         args = ['file']
-        res = parse_args(args)
-        self.assertEquals(res.files, args)
+        opts = parse_args(args)
+        self.assertEquals(opts.files, args)
 
 
     def test_twofiles(self):
         args = ['file1', 'file2']
-        res = parse_args(args)
-        self.assertEquals(res.files, args)
+        opts = parse_args(args)
+        self.assertEquals(opts.files, args)
 
 
     def test_flagnfile(self):
         flags = ['-v']
         files = ['file']
         args = flags + files
-        res = parse_args(args)
-        self.assertEquals(res.files, files)
-        self.assertEquals(res.verbose, 1)
+        opts = parse_args(args)
+        self.assertEquals(opts.files, files)
+        self.assertEquals(opts.verbose, 1)
 
 
     def test_filenflag(self):
         flags = ['-v']
         files = ['file']
         args = files + flags
-        res = parse_args(args)
-        self.assertEquals(res.files, files)
-        self.assertEquals(res.verbose, 1)
+        opts = parse_args(args)
+        self.assertEquals(opts.files, files)
+        self.assertEquals(opts.verbose, 1)
 
 
     def test_flagnfiles(self):
         flags = ['-v']
         files = ['file1', 'file2', 'file3', 'file4']
         args = flags + files
-        res = parse_args(args)
-        self.assertEquals(res.files, files)
-        self.assertEquals(res.verbose, 1)
+        opts = parse_args(args)
+        self.assertEquals(opts.files, files)
+        self.assertEquals(opts.verbose, 1)
 
 
     def test_filesnflag(self):
         flags = ['-v']
         files = ['file']
         args = files + flags
-        res = parse_args(args)
-        self.assertEquals(res.files, files)
-        self.assertEquals(res.verbose, 1)
-
-
+        opts = parse_args(args)
+        self.assertEquals(opts.files, files)
+        self.assertEquals(opts.verbose, 1)
 
